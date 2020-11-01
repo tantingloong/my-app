b6ff3fe4376872079e18f8f4e4c41cf8b28505c5
tantingloong <54939580+tantingloong@users.noreply.github.com>
2020-11-01 09:42:46 +0800

    Use Context API

--- src/AddressForm.js
+++ src/AddressForm.js
@@ -4,12 +4,37 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import {MyContext} from './MyContext'; //Context API
 
 export default function AddressForm() {
+  const [measure, setMeasure] = React.useContext(MyContext); //Context API
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+    console.log("got it - handleChange", name, value);
+
+    if (name === "firstTime") {
+      const { checked } = event.target;
+      setMeasure({
+        ...measure,
+        [name]: checked,
+      });
+    } else {
+      setMeasure({
+        ...measure,
+        [name]: value,
+      });
+    }
+
+    
+  };
+
+  
+
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
-        Welcome to Joy Studio
+        Welcome to Joy Studio {measure.firstName}
       </Typography>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={6}>
@@ -20,6 +45,8 @@ export default function AddressForm() {
             label="First name"
             fullWidth
             autoComplete="given-name"
+            defaultValue={measure.firstName}
+            onChange={handleChange}
           />
         </Grid>
         <Grid item xs={12} sm={6}>
@@ -30,71 +57,17 @@ export default function AddressForm() {
             label="Last name"
             fullWidth
             autoComplete="family-name"
+            defaultValue={measure.lastName}
+            onChange={handleChange}
           />
         </Grid>
-        {/* <Grid item xs={12}>
-          <TextField
-            required
-            id="address1"
-            name="address1"
-            label="Address line 1"
-            fullWidth
-            autoComplete="shipping address-line1"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            id="address2"
-            name="address2"
-            label="Address line 2"
-            fullWidth
-            autoComplete="shipping address-line2"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="city"
-            name="city"
-            label="City"
-            fullWidth
-            autoComplete="shipping address-level2"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="state"
-            name="state"
-            label="State/Province/Region"
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="zip"
-            name="zip"
-            label="Zip / Postal code"
-            fullWidth
-            autoComplete="shipping postal-code"
-          />
-        </Grid> */}
-        {/* <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="country"
-            name="country"
-            label="Country"
-            fullWidth
-            autoComplete="shipping country"
-          />
-        </Grid> */}
         <Grid item xs={12}>
           <FormControlLabel
             control={
-              <Checkbox color="secondary" name="firstTime" value="yes" />
+              <Checkbox color="primary" name="firstTime" checked={measure.firstTime} onChange={handleChange} >
+              </Checkbox>
             }
-            label="This is my first check-in with Coach Felicia"
+            label="This is my first check-in with Coach Felicia" 
           />
         </Grid>
       </Grid>
